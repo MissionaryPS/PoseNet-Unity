620b94c91d1d259e3ca9485f6c3e427f8420753c
TakaE623 <51111367+TakaE623@users.noreply.github.com>
2020-07-03 09:29:12 +0900

    add file PoseReflector

---
+++ Assets/Scripts/PoseReflector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseReflector : MonoBehaviour
+{
+    [SerializeField]
+    GameObject humanoid;    //unity humanoid準拠であればなんでもよい
+
+    Animator animator = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = humanoid.GetComponent<Animator>();
+    }
+
+    void PoseNet2VRM(PoseNet.Pose[] poses)
+    {
+
+    }
+
+}
